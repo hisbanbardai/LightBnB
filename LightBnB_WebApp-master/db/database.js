2493a74c476bcd0c451b15826bc1c5964110ba3c
Hisban Shiraz <hisbanbardai@gmail.com>
2023-11-15 12:55:41 -0700

    Implemented query code for getUserWithEmail function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -21,14 +21,13 @@ const pool = new Pool(config);
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool.query(`select * from users where email = $1`, [email])
+    .then((result) => {
+      return result.rows.length ? result.rows[0] : null;
+    })
+    .catch((err) => {
+      return err;
+    });
 };
 
 /**
@@ -37,7 +36,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  // return pool
+  //   .query(`select * from users where id = $1`, [id])
+  //   .then((result) => {
+  //     return result.rows.length ? result.rows[0] : null;
+  //   })
+  //   .catch((err) => {
+  //     return err;
+  //   });
 };
 
 /**
@@ -72,15 +78,13 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // This works because .then always returns a promise. Even though we wrote the line return result.rows (where result.rows is an array of objects), .then automatically places that value in a promise. .then returns a promise, which is returned as a result of the entire getAllProperties function.
   return pool
     .query(`select * from properties limit $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
+      return err;
     });
 };
 
